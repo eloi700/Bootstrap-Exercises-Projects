8d65e48fce51c31cc9f9a2ccfc8d05adf87427a8
Cielo <macielo.tablate@gmail.com>
2022-07-11 13:49:46 +0200

    add core

--- pass-counter/index.js
+++ pass-counter/index.js
@@ -1,12 +1,15 @@
 let count = 0;
+let saveEl = document.getElementById('saveEl');
 const countEl = document.getElementById('count-el');
 
 function increment(){
-    count ++;
+    count += 1;
     countEl.textContent = count;
 }
 
 function save(){
+    let saveCount = count + ',' ;
+    saveEl.textContent += saveCount;
     count = 0;
     countEl.textContent = count;
 }
